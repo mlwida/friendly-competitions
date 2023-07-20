cbb09a6682f9fbf10b9a4c709770f0b13cab22be
Steffen Springer <springer@epoq.de>
2023-07-20 13:20:31 +0200

    Getting started

---
+++ pumpitup/README.md
@@ -0,0 +1,2 @@
+# Pump it up 
+https://www.drivendata.org/competitions/7/pump-it-up-data-mining-the-water-table/
---
+++ pumpitup/src/pumpitup/playground.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+if __name__ == '__main__':
+    print("Hello World !")
+    print("I am bored, when do we start ?")
